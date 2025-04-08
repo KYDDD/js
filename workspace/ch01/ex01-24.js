ab49ef79868c87bcb725b4a11765531c9030860e
yeono <126737362+KYDDD@users.noreply.github.com>
2025-04-08 22:23:52 +0900

    객체

--- workspace/ch01/ex01-24.js
+++ workspace/ch01/ex01-24.js
@@ -2,3 +2,18 @@
 JSON 표기법 - 리터럴 방식으로 객체 생성
 */
 
+/*
+JSON 표기법 - 리터럴 방식으로 객체 생성
+JavaScript Object Notation -> 자바스크립트 객체 표기법법
+*/
+
+const foo = {
+  name: "제이슨핑",
+  age: 7,
+  job: "요정",
+  married: true,
+};
+
+console.log(foo["name"], foo["age"], foo.job, foo.married);
+//object {name: '함수핑', age: 6, job: '마법사', married: false}
+console.log(typeof foo, foo);
