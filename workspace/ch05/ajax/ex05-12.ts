87fe3514dca028d66e9f3d58d51f8a07b8f9aca2
yeono <126737362+KYDDD@users.noreply.github.com>
2025-05-08 11:39:27 +0900

    ajax

--- workspace/ch05/ajax/ex05-12.ts
+++ workspace/ch05/ajax/ex05-12.ts
@@ -1,7 +1,22 @@
 // 랜덤 고양이 이미지 가져오기 - XMLHttpRequest 사용
+const url = "https://api.thecatapi.com/v1/images/search";
 
-const url = 'https://api.thecatapi.com/v1/images/search';
+const btn = document.querySelector("button");
 
-function getImages(){
-  
-};
+btn?.addEventListener("click", getImages);
+
+function getImages(): void {
+  // Ajax 프로그래밍 순서
+  // 1. XMLHTTPRequest
+  const xhr = new XMLHttpRequest();
+
+  // 2. 요청 준비(open)
+  xhr.open("GET", url, false);
+  // 4. 응답 데이터 처리
+  xhr.addEventListener("load", function () {
+    const result = this.responseTest;
+    console.log(result);
+  });
+  // 3. 서버에 요청(send)
+  xhr.send();
+}
