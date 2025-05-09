28285fb3dd80446c32fec9e2e51083b89e4609d8
yeono <126737362+KYDDD@users.noreply.github.com>
2025-05-10 00:44:24 +0900

    웹소켓

--- workspace/ch05/ajax/ex05-12.js
+++ workspace/ch05/ajax/ex05-12.js
@@ -1,4 +1,4 @@
-"use strict";
+import { appendImages } from "./utils.js";
 // 랜덤 고양이 이미지 가져오기 - XMLHttpRequest 사용
 const url = "https://api.thecatapi.com/v1/images/search";
 const btn = document.querySelector("button");
@@ -8,11 +8,14 @@ function getImages() {
     // 1. XMLHTTPRequest
     const xhr = new XMLHttpRequest();
     // 2. 요청 준비(open)
-    xhr.open("GET", url, false);
+    xhr.open("GET", url, true);
     // 4. 응답 데이터 처리
     xhr.addEventListener("load", function () {
-        const result = this.responseTest;
+        const result = this.responseText;
         console.log(result);
+        const data = JSON.parse(result);
+        console.log(data);
+        appendImages(data);
     });
     // 3. 서버에 요청(send)
     xhr.send();
--- workspace/ch05/ajax/ex05-12.ts
+++ workspace/ch05/ajax/ex05-12.ts
@@ -1,3 +1,6 @@
+import { type Cat } from "./types.js";
+import { appendImages } from "./utils.js";
+
 // 랜덤 고양이 이미지 가져오기 - XMLHttpRequest 사용
 const url = "https://api.thecatapi.com/v1/images/search";
 
@@ -11,11 +14,14 @@ function getImages(): void {
   const xhr = new XMLHttpRequest();
 
   // 2. 요청 준비(open)
-  xhr.open("GET", url, false);
+  xhr.open("GET", url, true);
   // 4. 응답 데이터 처리
   xhr.addEventListener("load", function () {
-    const result = this.responseTest;
+    const result = this.responseText;
     console.log(result);
+    const data: Cat[] = JSON.parse(result);
+    console.log(data);
+    appendImages(data);
   });
   // 3. 서버에 요청(send)
   xhr.send();
