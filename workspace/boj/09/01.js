ce83167921d4789eede1494cdd5f19b03f24d12f
김연호 <riderkim88@naver.com>
2025-10-03 14:25:57 +0900

    5086

--- workspace/boj/09/01.js
+++ workspace/boj/09/01.js
@@ -36,3 +36,32 @@ multiple
 neither
 */
 
+function main() {
+  const data = getData();
+
+  for (let i = 0; i < data.length - 1; i++) {
+    if (data[i][1] % data[i][0] === 0) {
+      console.log("factor");
+    } else if (data[i][0] % data[i][1] === 0) {
+      console.log("multiple");
+    } else {
+      console.log("neither");
+    }
+  }
+}
+main();
+
+function getData() {
+  const arr = require("fs").readFileSync(0).toString().trim().split("\n");
+  const result = [];
+
+  for (let row of arr) {
+    const rowArr = row.split(" ");
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+    result.push(rowArr.length === 1 ? rowArr[0] : rowArr);
+  }
+
+  return result.length === 1 ? result[0] : result;
+}
