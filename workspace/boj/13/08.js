3d7247786a86264db63ac4ceb775bcc296d8589b
김연호 <riderkim88@naver.com>
2025-10-19 22:52:13 +0900

    11650

--- workspace/boj/13/08.js
+++ workspace/boj/13/08.js
@@ -6,3 +6,34 @@
 
 */
 
+function main() {
+  const data = getData();
+  const number = data[0];
+  const sliceData = data.slice(1, 1 + number);
+
+  const sortedData = sliceData.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    } else {
+      return a[0] - b[0];
+    }
+  });
+
+  sortedData.forEach((item) => console.log(...item));
+}
+main();
+
+function getData() {
+  const arr = require("fs").readFileSync(0).toString().trim().split("\n");
+  const result = [];
+
+  for (let row of arr) {
+    const rowArr = row.split(" ");
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+    result.push(rowArr.length === 1 ? rowArr[0] : rowArr);
+  }
+
+  return result.length === 1 ? result[0] : result;
+}
